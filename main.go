915ceb051cbd63aad4a1715795ec0fa3db71057f
Ciro García Belmonte <info@cirogarcia.dev>
2025-12-01 07:47:19 +0100

    chore(build): enhance cross-platform build configuration - Implemented a matrix strategy for building on Linux and macOS - Added conditional steps for macOS dependencies and installer creation - Updated build commands to support multiple platforms

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 	"io/fs"
 	"net/http"
@@ -115,6 +116,9 @@ func main() {
 			WindowIsTranslucent: true,
 		},
 		Linux: &linux.Options{
+			WindowIsTranslucent: true,
+		},
+		Mac: &mac.Options{
 			WindowIsTranslucent: true,
 		},
 		EnumBind: []any{
